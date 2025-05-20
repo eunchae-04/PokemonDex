58ece403266e8fcb213f7758fea771c8ff28c9e2
eunchae-04 <rhdmsco1163@naver.com>
2025-05-20 20:23:26 +0900

    third commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/pokemon/',  // GitHub 저장소 이름과 똑같이
+  base: '/PokemonDex/',  // GitHub 저장소 이름과 똑같이
   plugins: [react()],
 })
