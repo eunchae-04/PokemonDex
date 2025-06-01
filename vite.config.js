768e7030937f9435a790afe8cb12845532816e07
eunchae-04 <rhdmsco1163@naver.com>
2025-06-01 15:30:24 +0900

    prop-drilling 방식 구현

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/PokemonDex/',  // GitHub 저장소 이름과 똑같이
+  base: '/PokemonDex/',  // 저장소 이름과 정확히 일치해야 함
   plugins: [react()],
 })
